7f402ebdf00203711a3155e4b4edd618f99532d0
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-10-07 21:05:34 -0300

    basic environment configuration

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-magic-numbers': ['warn', { ignore: [1, 0] }],
+    'no-console': 'warn',
+    'no-undef': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-no-undef': 'off',
+  },
+};
